514bf85cdd33d4d4f925c62d92edfda49163cea9
YanchunZhou <tifa1981@gmail.com>
2022-01-14 11:48:08 -0500

    Shapes-Square

---
+++ src/day39_Recap/shapeTask/Rectangle.java
@@ -0,0 +1,58 @@
+package day39_Recap.shapeTask;
+
+public class Rectangle extends Shape {
+
+    private double length, width;
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        if(length <= 0){
+            System.err.println("Invalid length: "+ length);
+            System.exit(1);
+
+        }
+        this.length = length;
+    }
+
+    public double getWidth() {
+        if(width <= 0){
+            System.err.println("Invalid Width: "+width);
+            System.exit(1);
+        }
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public Rectangle( double length, double width) {
+        super("Rectangle");
+        setLength(length);
+        setWidth(width);
+
+
+    }
+
+    @Override
+    public double area() {
+        return length * width;
+    }
+
+    @Override
+    public double perimeter() {
+        return  2*(length + width);
+    }
+    public String toString() {
+        return "Rectangle{" +
+                "length=" + length +
+                ", width=" + width +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter() + '\'' +
+                '}';
+    }
+
+}
